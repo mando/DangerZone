bc50e990ef4e9a959f35a3ba5c8e7fb4da913a49
Mando Escamilla <mando@mando.org>
2013-03-28 00:35:50 -0500

    Roll that beautiful title

--- js/app.js
+++ js/app.js
@@ -3,3 +3,21 @@ App = Ember.Application.create({});
 App.Router.map(function() {
   this.resource('slides');
 });
+
+App.Slide = Ember.Object.extend({
+  title: null,
+  bullets: null,
+  id: null
+});
+
+var slide_1 = App.Slide.create({
+  title: "Getting started with Ember"
+});
+
+App.SlidesRoute = Ember.Route.extend({
+  model: function() {
+    return slide_1;
+  }
+});
+
+
