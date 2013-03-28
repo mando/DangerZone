9a30181f1e67f05512b57e6d14eded7736fc5107
Mando Escamilla <mando@mando.org>
2013-03-28 00:49:33 -0500

    And here is where it all starts to fall apart.

--- js/app.js
+++ js/app.js
@@ -1,28 +1,40 @@
 App = Ember.Application.create({});
 
 App.Router.map(function() {
-  this.resource('slides');
+  this.resource('slides', { path: '/slides/:slide_id' });
 });
+
+App.Slides = Ember.A([]);
 
 App.Slide = Ember.Object.extend({
   title: null,
   bullets: null,
-  id: null
+  id: null,
+  init: function() {
+    App.Slides.pushObject(this)
+  }
 });
 
-var slide_1 = App.Slide.create({
+App.Slide.create({
+  id: 1,
   title: "Getting started with Ember",
   bullets: ['Mando Escamilla', 'github.com/mando']
 });
 
+App.Slide.create({
+  id: 2,
+  title: "Now it's SERIOUS"
+});
+
+
 App.SlidesRoute = Ember.Route.extend({
-  model: function() {
-    return slide_1;
+  model: function(params) {
+    return App.Slides.findProperty('id', parseInt(params.slide_id))
   }
 });
 
 App.IndexRoute = Ember.Route.extend({
   redirect: function() {
-    this.transitionTo("slides");
+    this.transitionTo("slides", App.Slides[0]);
   }
 });
